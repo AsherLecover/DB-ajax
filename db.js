45fd74892a8cabde0105c4679105931c4d97a0ad
AsherLecover <asherlec5@gmail.com>
2020-06-24 19:21:48 +0300

    initail commit

---
+++ db.js
@@ -0,0 +1,8 @@
+let someDB = {
+    "students":[
+        { name:"asher 1", age : 35},
+        { name:"asher 2", age : 22},
+        { name:"asher 3", age : 44},
+        { name:"asher 4", age : 126},
+    ]
+}
